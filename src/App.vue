fd87fb2c4d3e0f735cfe52a15527951acf05b070
joli <1040123159@qq.com>
2020-09-14 21:39:15 +0800

    模块化：Money.vue分为四个子组件，并写好了html和css

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
-<template>
-  <div>
-    <router-view/>
-  </div>
-</template>
+ <template>
+    <div id="app">
+      <router-view/>
+    </div>
+  </template>
 
-<style lang="scss">
-@import "~@/assets/style/reset.scss";
-
-body{
-  line-height: 1.5;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-</style>
+  <style lang="scss">
+    @import "~@/assets/style/helper.scss";
+    @import "~@/assets/style/reset.scss";
+    body{
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      color: #333;
+      font-family: $font-hei;
+      font-size: 16px;
+      line-height: 1.5;
+    }
+  </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
+
+
 <template>
   <nav>
     <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-
     <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-
     <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
@@ -18,9 +18,6 @@
 </template>
 
 <script lang="ts">
-
-const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {importAll(require.context('../assets/icons/', true, /\.svg$/));} catch (error) {console.log(error);}
 export default {
   name: 'Nav'
 };
@@ -29,8 +26,8 @@ export default {
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
 nav {
+  @extend %outerShadow;
   display: flex;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25 );
   flex-direction: row;
   font-size: 12px;
   > .item {
@@ -40,13 +37,14 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
   }
-  .icon{
-    width: 32px;
-    height: 32px;
-  }
-  >.item.selected{
+  > .item.selected{
     color: $color-highlight;
   }
 }
 </style>
+=
