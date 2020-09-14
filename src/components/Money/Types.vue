4254c891d4fc9dac3551803826c5eee7aa6925ef
joli <1040123159@qq.com>
2020-09-14 21:51:42 +0800

    使用js使用了一个组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type==='-'&&'selected'"
+      @click="selectType('-')">支出</li>
+      <li  :class="type==='+'&&'selected'"
+           @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 export default {
-  name: 'Types'
+  name: 'Types',
+  data(){
+    return {
+      type:'-' // '-'表示支出，'+'表示收入
+    }},
+  methods:{
+    selectType(type){
+      if(type!=='-'&&type!=='+'){
+        throw new Error('type id unknown')
+      }
+      this.type=type
+    }
+  }
 };
 </script>
 
